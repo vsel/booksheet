028fe9fc7b7db3f0097f432a9ce3f8b7fbf60173
Volodymyr Selyukh <kotshynona@ukr.net>
2015-05-03 21:17:39 +0000

    Authentication with BasicHTTPAuth and Tokens in REST API added

--- server/bss/api_1_0/bookmarks.py
+++ server/bss/api_1_0/bookmarks.py
@@ -1,13 +1,23 @@
-from flask import jsonify, request
+from flask import jsonify, request, g, abort, url_for, current_app
 from .. import db
 from . import api
 from ..dao import Bookmark
+from flask.ext.login import login_required
+
+@api.route('/bookmarks/add', methods=['POST'])
+def add_new_bookmark_with_json():
+    #print request.json
+    bookmark = Bookmark.from_json(request.json)
+    db.session.add(bookmark)
+    db.session.commit()
+    return jsonify(bookmark.to_json()) 
 
 @api.route('/bookmarks/get_all', methods=['GET'])
 def get_all_bookmarks():
     #print request.json
+    print(g.current_user.id)
     json_bookmarks_array=[]
-    bookmarks = Bookmark.query.filter_by(userid=1).order_by('timestamp desc').all()
+    bookmarks = Bookmark.query.filter_by(userid=g.current_user.id).order_by('timestamp desc').all()
     for bookmark in bookmarks:
         json_bookmarks_array.append(bookmark.to_json())
     #print json_bookmarks_array
