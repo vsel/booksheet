acd9321737158b0b3e7415a9d9be56031fcf618f
Volodymyr Selyukh <kotshynona@ukr.net>
2015-05-20 09:44:56 +0000

    AWS Ready =)

--- server/application.py
+++ server/application.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import os
-from bss import create_app, db
-from flask.ext.script import Manager, Shell, Server
-from flask.ext.migrate import Migrate, MigrateCommand
+from bss import create_app
+from flask.ext.script import Server
+
 
 bss = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
