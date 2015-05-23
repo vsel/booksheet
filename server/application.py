1c19cf04d7c8b1f4b5c8b0d06d6553c236ef2dcb
vsel <volodymyr.volodymyrovych@gmail.com>
2015-05-23 18:42:53 +0300

    AWS Ready =)

--- server/application.py
+++ server/application.py
@@ -4,8 +4,8 @@ from bss import create_app
 from flask.ext.script import Server
 
 
-bss = create_app(os.getenv('FLASK_CONFIG') or 'default')
+application = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 if __name__ == '__main__':
     bss.debug = True
-    bss.run(host="0.0.0.0", port=8080)
+    bss.run()
